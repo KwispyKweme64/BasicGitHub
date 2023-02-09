c47d282808f3d12fab23636d80ba5bcf32d3923f
andre <andre@DESKTOP-K2MU70E>
2023-02-09 12:11:06 -0600

    Add project files.

---
+++ AH Basic App GitHub/Basic WebForm3.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Basic WebForm3.aspx.cs" Inherits="AH_Basic_App_GitHub.Basic_WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <% AH_Basic_App_GitHub.Class1 tp = new AH_Basic_App_GitHub.Class1(); %>
+            <% =tp.Name; %>
+        </div>
+    </form>
+</body>
+</html>
---
+++ BasicAppTest3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicAppTest3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicAppTest3")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f39c21c9-0b50-402e-8b75-fca9a7b26a58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
